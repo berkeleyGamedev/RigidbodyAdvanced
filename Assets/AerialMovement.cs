c601f2f912d6006a88e0f23bc7e039fb819e3e1c
Weylan Wang <weylan.wang@berkeley.edu>
2019-01-08 16:15:51 -0800

    Do you wanna build a snowman

---
+++ Assets/AerialMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AerialMovement : MonoBehaviour {
+    Rigidbody2D robot;
+    float xAxis;
+    public bool canJump;
+
+    // Use this for initialization
+    void Start () {
+        robot = GetComponent<Rigidbody2D>();
+        canJump = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        move1();
+        if (Input.GetKeyDown(KeyCode.W) && canJump == true) {
+            jump();
+        }
+	}
+
+    void move1() {
+        //Fill in whatever x axis movement you'd like
+       
+    }
+
+    void jump() {
+        //robot.velocity = new Vector2(robot.velocity.x, 7);
+        //canJump = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
+
+}
---
+++ Assets/GroundMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour {
+    Rigidbody2D robot;
+    float xAxis;
+
+	// Use this for initialization
+	void Start () {
+        robot = GetComponent<Rigidbody2D>();
+   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        xAxis = Input.GetAxisRaw("Horizontal");
+
+        //Keep in mind if you want custom movement controls you can always do things like this:
+
+        //left = Input.GetKeyDown(KeyCode."Insert Your Key Here");
+        //and then follow it up with other lines 
+
+        //or something like this:
+
+        //bool movingRight = false;
+        //if (Input.GetKeyDown(KeyCode."Insert Button Here")):
+        //      movingRight = true;
+        //else:
+        //      movingRight = false;
+        //if (movingRight) {
+        //    do stuff  
+        //  }
+
+        move1();     
+    }
+
+    void move1() {
+        Vector2 movement = new Vector2(xAxis, 0);
+        robot.AddForce(movement);
+    }
+
+    void move2() {
+        //use Mathf.Clamp()
+        Vector2 movement = new Vector2(xAxis, 0);
+        movement = movement * 150;
+        robot.AddForce(movement);
+        robot.velocity = new Vector2(Mathf.Clamp(robot.velocity.x, 0, 3), robot.velocity.y);
+    }
+
+    void move3() {
+        //Make a movement function that instantaneously moves the robot right when "D" is pressed, and 
+        //instantaneously moves the robot left when "A" is pressed. Robot should immediately stop if no 
+        //button is pressed. 
+    }
+
+    void translate1() {
+        Vector2 movement = new Vector2(xAxis, 0);
+        robot.MovePosition(robot.position + movement);
+    }
+}
